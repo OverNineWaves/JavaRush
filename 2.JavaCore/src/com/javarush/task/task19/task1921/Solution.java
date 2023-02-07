03116ac8d36d6a931e12b9227eed71d5475dab52
Dmitrii Frolov <tryfrod@gmail.com>
2023-02-07 15:40:55 +0400

    ипользование регулярного выражения,чтобы отсечь лишнее.

--- 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1921/Solution.java
@@ -1,8 +1,11 @@
 package com.javarush.task.task19.task1921;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
@@ -14,8 +17,26 @@ import java.util.List;
 
 public class Solution {
     public static final List<Person> PEOPLE = new ArrayList<Person>();
+    static BufferedReader bufferedFileReader;
+    static SimpleDateFormat dataFormat = new SimpleDateFormat("dd MM yyyy");
 
     public static void main(String[] args) {
-
+        try {
+            bufferedFileReader = new BufferedReader(new FileReader(args[0]));
+            while(bufferedFileReader.ready()){
+            String str = bufferedFileReader.readLine();
+                try {
+                    PEOPLE.add(new Person(str.replaceAll("[^а-яА-Я ]","").trim(), dataFormat.parse(str.replaceAll("[^0-9 ]", "").trim())));
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            bufferedFileReader.close();
+        }  catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+       /* for (int i = 0; i <PEOPLE.size() ; i++) {
+            System.out.println(PEOPLE.get(i).getName() + " " + PEOPLE.get(i).getBirthDate());
+        }*/
     }
 }
