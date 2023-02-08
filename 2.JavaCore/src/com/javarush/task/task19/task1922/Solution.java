2b4d3345e10f0936bde93fd5c0c4aa170c303464
Dmitrii Frolov <tryfrod@gmail.com>
2023-02-08 15:17:28 +0400

    ищем совпадения в строке по содержимому из массива. если есть не менее и не более 2 совпадений, выводим. учитываем регистр.

--- 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1922/Solution.java
@@ -13,6 +13,9 @@ import java.util.List;
 
 public class Solution {
     public static List<String> words = new ArrayList<String>();
+    static BufferedReader bufferedFileNameReader;
+    static BufferedReader bufferedFileReader;
+    static String fileName;
 
     static {
         words.add("файл");
@@ -21,6 +24,30 @@ public class Solution {
     }
 
     public static void main(String[] args) {
+            bufferedFileNameReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            fileName = bufferedFileNameReader.readLine();
+            bufferedFileNameReader.close();
+            bufferedFileReader = new BufferedReader(new FileReader(fileName));
+
+            while (bufferedFileReader.ready()){
+                String strLine = bufferedFileReader.readLine();
+                String[] strLineArray = strLine.split(" ");
+                int count = 0;
+                for (int i = 0; i < words.size() ; i++) {
+                    for (int j = 0; j < strLineArray.length; j++) {
+                        if (words.get(i).equals(strLineArray[j])){
+                            count++;}
+                    }
+                }
+                if (count == 2){
+                    System.out.println(strLine);
+                }
+            }
+            bufferedFileNameReader.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
     }
 }
