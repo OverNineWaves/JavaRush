2805390e0202e2a6ac3de7824e98ea2d4beedd0a
Dmitrii Frolov <tryfrod@gmail.com>
2023-02-23 16:25:03 +0400

    выглядит как говнокод, но работает. нужно через каждые 2 вывода с метода printSomething вывести свою строку с текстом.

--- 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task19/task1927/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task19.task1927;
 
 import java.io.ByteArrayOutputStream;
+import java.io.OutputStream;
 import java.io.PrintStream;
 
 /* 
@@ -11,6 +12,33 @@ public class Solution {
     public static TestString testString = new TestString();
 
     public static void main(String[] args) {
+        PrintStream trueOut = System.out;
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(byteArrayOutputStream);
+
+        System.setOut(printStream);
+        testString.printSomething();
+
+        System.setOut(trueOut);
+
+        int count = 1;
+        int numb =  0;
+        String[] str = byteArrayOutputStream.toString().split("\n");
+        for (int i = 0; i < str.length + 2; i++) {
+            if (count % 3 == 0){
+                //System.out.println(str[i]);
+                System.out.println("JavaRush - курсы Java онлайн");
+                count = 1;
+            }
+
+            else {
+            System.out.println(str[numb]);
+            numb++;
+            count++;}
+
+        }
+
     }
 
     public static class TestString {
