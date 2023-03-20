f2abdabf4e3003e35370cf9eee47fd255ebd0380
Dmitrii Frolov <tryfrod@gmail.com>
2023-03-20 15:21:06 +0400

    немного изучаем класс properties

--- 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2003/Solution.java
@@ -14,21 +14,41 @@ public class Solution {
     public static Map<String, String> runtimeStorage = new HashMap<>();
 
     public static void save(OutputStream outputStream) throws Exception {
-        //напишите тут ваш код
+        Properties propertiesSave = new Properties();
+        propertiesSave.putAll(runtimeStorage);
+        propertiesSave.store(outputStream, null);
+        //реализовать логику записи в файл для карты runtimeStorage
+
     }
 
     public static void load(InputStream inputStream) throws IOException {
-        //напишите тут ваш код
+        //реализовать логику чтения из файла для runtimeStorage
+        Properties propertiesLoad = new Properties();
+        propertiesLoad.load(inputStream);
+
+        for (String  name : propertiesLoad.stringPropertyNames()){
+            runtimeStorage.put(name, propertiesLoad.getProperty(name));
+        }
+
+        /*for(Map.Entry<String, String> runMap : runtimeStorage.entrySet()){
+            System.out.println(runMap.getKey() + " <key | value> " + runMap.getValue());
+        }*/
     }
 
     public static void main(String[] args) {
         try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-             FileInputStream fos = new FileInputStream(reader.readLine())) {
-            load(fos);
+             FileInputStream fis = new FileInputStream(reader.readLine());
+             //FileOutputStream fos = new FileOutputStream(reader.readLine(),true);
+        ) {
+            load(fis);
+            /*try {
+                save(fos);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }*/
         } catch (IOException e) {
             e.printStackTrace();
         }
-
         System.out.println(runtimeStorage);
     }
 }
