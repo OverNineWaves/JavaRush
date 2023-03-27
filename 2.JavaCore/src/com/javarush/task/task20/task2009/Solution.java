cfb2742e91f49540c21ee786093b1c4130b818e3
Dmitrii Frolov <tryfrod@gmail.com>
2023-03-27 16:14:43 +0400

    должно быть так

--- 2.JavaCore/src/com/javarush/task/task20/task2009/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2009/Solution.java
@@ -1,5 +1,8 @@
 package com.javarush.task.task20.task2009;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 /* 
@@ -7,10 +10,18 @@ import java.io.Serializable;
 */
 
 public class Solution {
-    public static class ClassWithStatic {
+    public static class ClassWithStatic implements Serializable{
         public static String staticString = "This is a static test string";
         public int i;
         public int j;
+
+        public static void serializeStatic(ObjectOutputStream oos) throws IOException{
+            oos.writeUTF(staticString);
+        }
+
+        public static void deserializeStatic(ObjectInputStream ois) throws IOException{
+            staticString = ois.readUTF();
+        }
     }
 
     public static void main(String[] args) {
