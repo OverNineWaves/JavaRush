14eff5873c49af404a65eab37c3c0c664f9ff95a
Dmitrii Frolov <tryfrod@gmail.com>
2023-03-29 17:36:07 +0400

    серилка, но ручная

--- 2.JavaCore/src/com/javarush/task/task20/task2011/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2011/Solution.java
@@ -1,9 +1,6 @@
 package com.javarush.task.task20.task2011;
 
-import java.io.Externalizable;
-import java.io.IOException;
-import java.io.ObjectInput;
-import java.io.ObjectOutput;
+import java.io.*;
 
 /* 
 Externalizable для апартаментов
@@ -11,7 +8,7 @@ Externalizable для апартаментов
 
 public class Solution {
 
-    public static class Apartment {
+    public static class Apartment implements Externalizable {
 
         private String address;
         private int year;
@@ -34,9 +31,35 @@ public class Solution {
         public String toString() {
             return ("Address: " + address + "\n" + "Year: " + year);
         }
+
+        @Override
+        public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(address);
+        out.writeInt(year);
+        }
+
+        @Override
+        public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        address = (String) in.readObject();
+        year = in.readInt();
+        }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        String file = "file.dat";
+        ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
+        ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
+        //new Apartment("12", 12).writeExternal(oos);
+        Apartment a1 = new Apartment("adrees 1", 1885);
+        a1.writeExternal(oos);
+        oos.close();
+        try {
+            a1.readExternal(ois);
+            ois.close();
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(a1.toString());
 
     }
 }
