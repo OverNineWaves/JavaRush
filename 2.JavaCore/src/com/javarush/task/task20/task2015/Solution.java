a208977b242b4eb48beebbd70ff3ed37f6ee09f7
Dmitrii Frolov <tryfrod@gmail.com>
2023-03-30 18:47:35 +0400

    серилка

--- 2.JavaCore/src/com/javarush/task/task20/task2015/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2015/Solution.java
@@ -1,44 +1,61 @@
-//package com.javarush.task.task20.task2015;
-//
-//import java.io.IOException;
-//import java.io.ObjectInputStream;
-//import java.io.ObjectOutputStream;
-//import java.io.Serializable;
-//
-///*
-//Переопределение сериализации
-//*/
-//
-//public class Solution implements {
-//    private Thread runner;
-//    private int speed;
-//
-//    public Solution(int speed) {
-//        this.speed = speed;
-//        runner = new Thread(this);
-//        runner.start();
-//    }
-//
-//    public void run() {
-//        // do something here, doesn't matter what
-//    }
-//
-//    /**
-//     * Переопределяем сериализацию.
-//     * Для этого необходимо объявить методы:
-//     * private void writeObject(ObjectOutputStream out) throws IOException
-//     * private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
-//     * Теперь сериализация/десериализация пойдет по нашему сценарию :)
-//     */
-//    private void writeObject(ObjectOutputStream out) throws IOException {
-//        out.defaultWriteObject();
-//    }
-//
-//    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
-//        in.defaultReadObject();
-//    }
-//
-//    public static void main(String[] args) {
-//
-//    }
-//}
+package com.javarush.task.task20.task2015;
+
+import java.io.*;
+
+/*
+Переопределение сериализации
+*/
+
+public class Solution implements Serializable, Runnable {
+    private transient Thread runner;
+    private int speed;
+
+    public Solution(int speed) {
+        this.speed = speed;
+        runner = new Thread(this);
+        runner.start();
+    }
+
+    public void run() {
+        System.out.println("message");
+        System.out.println(Thread.currentThread());
+        // do something here, doesn't matter what
+    }
+
+    /**
+     * Переопределяем сериализацию.
+     * Для этого необходимо объявить методы:
+     * private void writeObject(ObjectOutputStream out) throws IOException
+     * private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException
+     * Теперь сериализация/десериализация пойдет по нашему сценарию :)
+     */
+    private void writeObject(ObjectOutputStream out) throws IOException {
+        out.defaultWriteObject();
+    }
+
+    private void readObject(ObjectInputStream in) throws IOException, ClassNotFoundException {
+        in.defaultReadObject();
+        Thread thread1 = new Thread(this);
+        thread1.start();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution(20);
+        try {
+            ByteArrayOutputStream byts = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(byts);
+            oos.writeObject(solution);
+            oos.close();
+
+            ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(byts.toByteArray()));
+            try {
+                Solution solution1 = (Solution) ois.readObject();
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+}
