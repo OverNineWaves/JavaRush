525337790b1f84f687a8bae4e946df9de79efc64
Dmitrii Frolov <tryfrod@gmail.com>
2023-03-30 16:17:11 +0400

    серилка

--- 2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2014/Solution.java
@@ -1,6 +1,6 @@
 package com.javarush.task.task20.task2014;
 
-import java.io.Serializable;
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -8,14 +8,32 @@ import java.util.Date;
 Serializable Solution
 */
 
-public class Solution {
+public class Solution implements Serializable{
     public static void main(String[] args) {
-        System.out.println(new Solution(4));
+        Solution savedObject = new Solution(4);
+        Solution loadedObject;
+        try {
+            ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("sol2014.dat"));
+            ObjectInputStream ois = new ObjectInputStream(new FileInputStream("sol2014.dat"));
+
+            oos.writeObject(savedObject);
+            try {
+               loadedObject = (Solution) ois.readObject();
+            } catch (ClassNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println(savedObject);
+        System.out.println(savedObject.string.equals(loadedObject.string));
+        //System.out.println(savedObject.toString());
+        //System.out.println(loadedObject.toString());
     }
 
-    private final String pattern = "dd MMMM yyyy, EEEE";
-    private Date currentDate;
-    private int temperature;
+    private transient final String pattern = "dd MMMM yyyy, EEEE";
+    private transient Date currentDate;
+    private transient int temperature;
     String string;
 
     public Solution(int temperature) {
