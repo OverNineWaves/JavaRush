2d6ac151c3bf7c1cb5351e0c1ffc31c64079f7dd
Dmitrii Frolov <tryfrod@gmail.com>
2023-04-07 14:31:50 +0400

    серилка

--- 2.JavaCore/src/com/javarush/task/task20/task2020/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2020/Solution.java
@@ -1,7 +1,6 @@
 package com.javarush.task.task20.task2020;
 
-import java.io.PrintStream;
-import java.io.Serializable;
+import java.io.*;
 import java.util.logging.Logger;
 
 /* 
@@ -10,15 +9,15 @@ import java.util.logging.Logger;
 
 public class Solution {
 
-    public static class Person {
+    public static class Person implements Serializable {
         String firstName;
         String lastName;
-        String fullName;
-        final String greeting;
+        transient String fullName;
+        transient final String greeting;
         String country;
         Sex sex;
-        PrintStream outputStream;
-        Logger logger;
+        transient PrintStream outputStream;
+        transient Logger logger;
 
         Person(String firstName, String lastName, String country, Sex sex) {
             this.firstName = firstName;
@@ -30,6 +29,11 @@ public class Solution {
             this.outputStream = System.out;
             this.logger = Logger.getLogger(String.valueOf(Person.class));
         }
+
+        public String toString(){
+            return ("firstName " + firstName + " lastName " + lastName + " fullName " + fullName + " greeting " + greeting + " country "+ country + " sex " + sex) ;
+        }
+
     }
 
     enum Sex {
@@ -37,7 +41,26 @@ public class Solution {
         FEMALE
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        String path = "sol2020.dat";
+        FileOutputStream fos = new FileOutputStream(path);
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+        Person persona = new Person("Vala", "Folla", "Brasil", Sex.FEMALE);
+
+        oos.writeObject(persona);
+        fos.close();
+        oos.close();
+        //System.out.println(persona);
+
+        /*FileInputStream fis = new FileInputStream(path);
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        Person personaIsBack = (Person) ois.readObject();
+        //personaIsBack = new Person(personaIsBack.firstName, personaIsBack.lastName, personaIsBack.country, personaIsBack.sex);
+
+        fis.close();
+        ois.close();
+        System.out.println(personaIsBack);*/
 
     }
 }
