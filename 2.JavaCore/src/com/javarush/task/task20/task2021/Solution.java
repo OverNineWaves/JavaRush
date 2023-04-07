045f09c093b4f7c1e6a81cbb4641efa181640f5f
Dmitrii Frolov <tryfrod@gmail.com>
2023-04-07 14:58:01 +0400

    серилка

--- 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task20/task2021/Solution.java
@@ -8,6 +8,14 @@ import java.io.*;
 
 public class Solution implements Serializable {
     public static class SubSolution extends Solution {
+
+        private void writeObject(ObjectOutputStream oos) throws NotSerializableException {
+            throw new NotSerializableException("бум");
+        }
+
+        private void readObject(ObjectInputStream ois) throws NotSerializableException {
+            throw new NotSerializableException("бац");
+        }
     }
 
     public static void main(String[] args) {
