e8697598c8509db81af3a29f6016813ca7201c00
Dmitrii Frolov <tryfrod@gmail.com>
2023-05-01 14:46:59 +0400

    28_24 изменил, мое решение не принимал.

--- 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1824/Solution.java
@@ -1,21 +1,45 @@
-package com.javarush.task.task18.task1824;
+/*package com.javarush.task.task18.task1824;
 
 import java.io.*;
-
+*/
 /* 
 Файлы и исключения
 */
 
-public class Solution {
+/*public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         while(true){
             String strRead = bufferedReader.readLine();
             File path = new File(strRead);
-                if (path.exists()){
-                    continue;
-                }
-                else throw new FileNotFoundException(strRead);
+            if (path.exists()){
+                continue;
+            }
+            else throw new FileNotFoundException(strRead);
+        }
+    }
+}*/
+package com.javarush.task.task18.task1824;
+
+
+import java.io.*;
+
+/*
+Файлы и исключения
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        while (true) {
+            String fileName = reader.readLine();
+            try (FileInputStream fileInputStream = new FileInputStream(fileName)) {
+
+            } catch (FileNotFoundException e) {
+                System.out.println(fileName);
+                break;
+            }
         }
     }
 }
