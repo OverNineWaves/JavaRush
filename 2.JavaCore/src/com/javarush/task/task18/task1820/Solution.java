7688dc13fc93fab5aba844d88f75afde49ad3e3a
Dmitrii Frolov <tryfrod@gmail.com>
2023-05-01 15:08:23 +0400

    28_20 пофиксил, видимо сонный делал, задание недопрочел

--- 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1820/Solution.java
@@ -12,80 +12,46 @@ import java.util.stream.Stream;
 public class Solution {
     public static void main(String[] args) {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
         try {
-            BufferedReader readFile = new BufferedReader(new FileReader(bufferedReader.readLine()));
+            String forRead = bufferedReader.readLine();
+            String forWrite = bufferedReader.readLine();
+            BufferedReader readFile = new BufferedReader(new FileReader(forRead));
+            PrintWriter printWriter = new PrintWriter(new FileWriter(forWrite));
             String readFromFile = readFile.readLine();
 
-            //FileReader fileOne = new FileReader(bufferedReader.readLine());
-            //Calculate.addNumb(fileOne);
-            //Calculate.calculate();
-
-            // FileWriter fileTwo = new FileWriter(bufferedReader.readLine());
-
             Calculate.arrayAdd(readFromFile);
-            Calculate.outputScreen();
-            // Calculate.readBuff(readFile);
-            // Calculate.printFile();
+            //Calculate.outputScreen();
+            Calculate.writeToFile(printWriter);
+
             bufferedReader.close();
             readFile.close();
-            //fileOne.close();
-            // fileTwo.close();
+            printWriter.close();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
 
-
-
     }
-    static class Calculate{
-        //static List<String> numb = new ArrayList<>();
+
+    static class Calculate {
         static String[] numb;
-        public static void arrayAdd(String str){
+
+        public static void arrayAdd(String str) {
             numb = str.split(" ");
         }
 
 
-        public static void outputScreen(){
-            for (int i = 0; i <numb.length ; i++) {
-                System.out.println(Math.round(Double.parseDouble(numb[i])));
-            }
-        }
-
-
-        /*public static void readBuff(BufferedReader bufferedReader) throws IOException {
-            while (bufferedReader.ready()){
-                numb.add(bufferedReader.readLine().split(" "));
-            }
-        }
-
-        public static void printFile(){
-            for (int i = 0; i <numb.size() ; i++) {
-                System.out.println(numb.get(i));
-            }
-        }*/
-
-
-        /*public static void addNumb(FileReader fileReader){
-               try{
-
-                   while (fileReader.ready()){
-                    double read = fileReader.read();
-                    numb.add(read);
-                }}
-               catch (IOException e){
-                   throw new RuntimeException(e);
-               }
-        }*/
-
-        /*public static void calculate(){
-            for (int i = 0; i < numb.size(); i++) {
-                    double n = numb.get(i);
-                if (n%((int)n) > 5 ){
-                    System.out.println("орп " + n);
+        /* public static void outputScreen(){
+             for (int i = 0; i <numb.length ; i++) {
+                 System.out.println(Math.round(Double.parseDouble(numb[i])));
+             }
+         }*/
+        public static void writeToFile(PrintWriter printWriter) {
+                 for (String numberStr : numb) {
+                    double number = Double.parseDouble(numberStr);
+                    long roundedNumber = Math.round(number);
+                    printWriter.print(roundedNumber + " ");
                 }
-                else System.out.println(n + " не опа");
             }
-        }*/
-
+        }
     }
-}
