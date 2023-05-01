f449d39de3942bf0d882861822452960a261cbc4
Dmitrii Frolov <tryfrod@gmail.com>
2023-05-01 14:49:10 +0400

    28_25 изменил, мое решение не принимал.

--- 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task18/task1825/Solution.java
@@ -1,14 +1,14 @@
-package com.javarush.task.task18.task1825;
+/*package com.javarush.task.task18.task1825;
 
 import java.io.*;
 import java.nio.file.Files;
 import java.util.*;
-
-/* 
+*/
+/*
 Собираем файл
 */
 
-public class Solution {
+/*public class Solution {
     public static void main(String[] args) throws IOException {
 
         Map<Integer, String> filesSortMap = new TreeMap<>();
@@ -27,16 +27,63 @@ public class Solution {
         //файла не существует, переписать.
         FileInputStream fileInputStream;
         for (Map.Entry<Integer, String> mapka: filesSortMap.entrySet()) {
-             fileInputStream = new FileInputStream(mapka.getValue());
-             //FileReader fileReader = new FileReader(mapka.getValue());
-                   while (fileInputStream.available() > 0){
-                       byte[] bytesBuffer = new byte[fileInputStream.available()];
-                       int count = fileInputStream.read(bytesBuffer);
-                    fileOutputStream.write(bytesBuffer);
-                    }
-             fileInputStream.close();
-                //System.out.println(mapka.getValue());
+            fileInputStream = new FileInputStream(mapka.getValue());
+            //FileReader fileReader = new FileReader(mapka.getValue());
+            while (fileInputStream.available() > 0){
+                byte[] bytesBuffer = new byte[fileInputStream.available()];
+                int count = fileInputStream.read(bytesBuffer);
+                fileOutputStream.write(bytesBuffer);
+            }
+            fileInputStream.close();
+            //System.out.println(mapka.getValue());
         }
         fileOutputStream.close();
+    }
+}*/
+package com.javarush.task.task18.task1825;
+
+import java.io.*;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.TreeSet;
+
+/*
+Собираем файл
+*/
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        Set<String> files = new TreeSet<>(new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                String[] parts1 = o1.split(".part");
+                String[] parts2 = o2.split(".part");
+                int number1 = Integer.parseInt(parts1[parts1.length - 1]);
+                int number2 = Integer.parseInt(parts2[parts2.length - 1]);
+                return number1 - number2;
+            }
+        });
+        String outputFile = null;
+        String readString;
+        while (!(readString = reader.readLine()).equals("end")) {
+            files.add(readString);
+            if (outputFile == null) {
+                int indexOfSuffix = readString.lastIndexOf(".part");
+                outputFile = readString.substring(0, indexOfSuffix);
+            }
+        }
+        if (outputFile == null) return;
+        try (FileOutputStream fileOutputStream = new FileOutputStream(outputFile)) {
+            for (String file : files) {
+                try (FileInputStream fileInputStream = new FileInputStream(file)) {
+                    byte[] buffer = new byte[fileInputStream.available()];
+                    while (fileInputStream.available() > 0) {
+                        int bytesRead = fileInputStream.read(buffer);
+                        fileOutputStream.write(buffer, 0, bytesRead);
+                    }
+                }
+            }
+        }
     }
 }
